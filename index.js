27a46693a9eaad7a9f33dd046962584d90e198ab
Karel Novak <novakk5@gmail.com>
2020-08-21 17:39:56 +0200

    add utils

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const {Queue} = require('./src/queues')
+const {Queue} = require('./src/queues');
+const {sleep} = require('./src/utils');
+
 
 module.exports.Queue = Queue;
+module.exports.sleep = sleep;
